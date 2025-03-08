2d275b66433561c922041345c7dd6f5078687e99
le.he <le.he@mintegral.com>
2025-03-09 01:29:14 +0800

    使用 next.js ssr 渲染

---
+++ pages/_document.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="zh-CN">
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="theme-color" content="#1890ff" />
+        <meta
+          name="description"
+          content="开发者工具箱 - 实用工具集合"
+        />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/logo192.png" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
